d15601fbd9fa1c87ed833dd5572f1c98c11d9f7f
Yuval Fatal <yuval.fatal@gmail.com>
2016-06-24 20:46:41 +0300

    Organize

--- node_server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-	console.log('request')
-  	res.jsonp({'message': 'Hello World! ' + req.query.name});
-});
-
-app.listen(3100);
