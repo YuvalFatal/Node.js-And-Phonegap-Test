b2a46d68b5a1be9280cec2bcda525747386e6c9c
Yuval Fatal <yuval.fatal@gmail.com>
2016-06-30 18:56:29 +0300

    twitter

--- Node.js & Phonegap/www/js/app.js
+++ Node.js & Phonegap/www/js/app.js
@@ -3,31 +3,55 @@
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     $.support.cors = true;
+    var socket = io.connect("http://10.0.0.11:3100");
+    var name = "yuval";
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
+    $('#submit').on('click', sendRequest);
+    socket.on('chat message', addMessage);
+    socket.on('twitt', addTwitt);
 
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        $('.name-list').append('<li><a href="#">Searching</a></li>');
-        $.ajax({
-            url: "http://10.0.0.4:3100",
-            type: "get",
-            data: {"name": $('.search-key').val()},
-            crossDomain: true,
-            dataType: "jsonp",
-            success: function(data, status) {
-                if(status != 'timeout')
-                    $('.name-list').append('<li><a href="#">' + data['message'] + '</a></li>');
-            },
-            complete: function(xhr,status){
-                $('.name-list').append('<li><a href="#">' + status + '</a></li>');
-            },
-            error: function(error) {
-                $('.name-list').append('<li><a href="#">' + error.message + '</a></li>');
-            }
-        });
+    function sendRequest(){
+        if($('#message').val().length == 0)
+            return false;
+
+        if($('#message').val().split(' ')[0] == "gt" && $('#message').val().split(' ').length > 1 && $('#message').val().split(' ')[1].length != 0){
+            var num_twitts = 1;
+
+            if($('#message').val().split(' ').length > 2 && !isNaN($('#message').val().split(' ')[2]))
+                num_twitts = parseInt($('#message').val().split(' ')[2]);
+
+            socket.emit('twitt', {name: 'test', user_name: String($('#message').val().split(' ')[1]), count: num_twitts});
+        } else {
+            socket.emit('chat message', {name: 'test', 'mes': $('#message').val()});
+        }
+
+        $('#message').val('');
+        return false;
+    }
+
+    function addMessage(msg){
+        if(msg['name'] == name)
+            $('#messages').append('<li id="myMsg">' + msg['mes'] + "</li>");
+        else
+            $('#messages').append($('<li>').html('<span>' + msg['name'] + '</span>: ' + msg['mes']));
+
+        focusOnLastElement();
+    }
+
+    function addTwitt(twitt){
+        if(twitt['name'] == name)
+            $('#messages').append('<li id="myMsg">' + twitt['num_twitt'] + twitt['html'] + '<script async src="lib/widgets.js" charset="utf-8"></script>' + "</li>");
+        else
+            $('#messages').append('<li><span>' + twitt['name'] + '</span>:' + twitt['num_twitt'] + twitt['html'] + '<script async src="lib/widgets.js" charset="utf-8"></script>' + "</li>");
+
+        focusOnLastElement();
+    }
+
+    function focusOnLastElement(){
+        $('html, body').scrollTop($("#messages li").last().offset().top);
     }
 
 }());
